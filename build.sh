b7d15f502d5117863c9de638d5ac4d69027a8f96
Jiale Zhi <vipcalio@gmail.com>
2020-04-20 13:44:45 -0700

    Create dir wheelhouse when build

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ PLAT=manylinux2010_x86_64
 #PLAT=manylinux1_x86_64
 PRE_CMD=
 
+mkdir -p wheelhouse
 docker pull $DOCKER_IMAGE
 docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/travis/build-wheels.sh
